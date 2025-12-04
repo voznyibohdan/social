7959df160b28229625c4db556cdc79aa5ffbae19
bohdanvoznyi <bohdanvoznyi.em@gmail.com>
2025-12-04 11:55:19 +0200

    Setting up the HTTP server and API

---
+++ cmd/api/api.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+}
+
+func (app *application) mount() http.Handler {
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.Timeout(60 * time.Second))
+
+	router.Route("/api/v1", func(r chi.Router) {
+		r.Get("/healthcheck", app.healthcheck)
+	})
+
+	return router
+}
+
+func (app *application) serve(handler http.Handler) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      handler,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	return srv.ListenAndServe()
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/voznyibohdan/social
 
 go 1.25.4
+
+require github.com/go-chi/chi/v5 v5.2.3 // indirect
